bc671a5a2548ebe844be989475ba85afb16a6d09
jef15938 <jef15938@gmail.com>
2021-05-21 21:18:06 +0800

    add canvas snow

---
+++ canvasSnow/main.js
@@ -0,0 +1,102 @@
+var canvasWidth = 1920;
+var canvasHeight = 1080;
+var numberOfParticles = 1000;
+var particleList = [];
+var canvas = $('#canvas')[0];
+var ctx = canvas.getContext('2d');
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+// class Particle
+/*
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvasWidth;
+    this.y = Math.random() * canvasHeight;
+    this.velocity = Math.random() * 3.5;
+    this.size = Math.random() * 3.5 + 1;
+  }
+  
+  update() {
+    this.y += this.velocity;
+    if(this.y > canvasHeight) {
+      this.y = 0;
+      this.x = Math.random() * canvasWidth;
+    }
+  }
+  
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = 'white';
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+  
+  
+}
+*/
+// class Particle end
+
+// for ie support class
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Particle = /*#__PURE__*/function () {
+  function Particle() {
+    _classCallCheck(this, Particle);
+
+    this.x = Math.random() * canvasWidth;
+    this.y = Math.random() * canvasHeight;
+    this.velocity = Math.random() * 3.5 + 0.5;
+    this.size = Math.random() * 3.5 + 1;
+  }
+
+  _createClass(Particle, [{
+    key: "update",
+    value: function update() {
+      this.y += this.velocity;
+
+      if (this.y > canvasHeight) {
+        this.y = 0;
+        this.x = Math.random() * canvasWidth;
+      }
+    }
+  }, {
+    key: "draw",
+    value: function draw() {
+      ctx.beginPath();
+      ctx.fillStyle = 'white';
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }]);
+
+  return Particle;
+}();
+// for ie support class end
+
+function initial() {
+  for (var i = 0; i < numberOfParticles; i++) {
+    particleList.push(new Particle());
+  }
+
+  console.log(particleList);
+}
+
+function animate() {
+  // ctx.globalAlpha = 1;
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  for (var i = 0; i < particleList.length; i++) {
+    particleList[i].update();
+    particleList[i].draw();
+  }
+
+  window.requestAnimationFrame(animate);
+}
+
+initial();
+animate();
