58d720bcdaf24b307a082ece0c2d738908a720d6
jef15938 <jef15938@gmail.com>
2021-06-05 22:47:02 +0800

    update

---
+++ canvasButtonEffect2/main.js
@@ -0,0 +1,134 @@
+var requestId = undefined;
+var canvasWidth = 150;
+var canvasHeight = 80;
+var canvasCenterX = canvasWidth / 2;
+var canvasCenterY = canvasHeight / 2;
+var numberOfParticles = 100;
+var particleList = [];
+var canvas = $('.button-container__canvas')[0];
+var ctx = canvas.getContext('2d');
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+// 每顆粒子
+var Particle = function (context, fillColor) {
+  this.context = context;
+  this.initial(fillColor);
+}
+
+// 初始化粒子
+Particle.prototype.initial = function (fillColor) {
+  this.x = canvasCenterX;
+  this.y = canvasCenterY;
+  this.opacity = 1;
+  this.velocityX = (Math.random() * 2) - 1;  // -1 ~ 1
+  this.velocityY = (Math.random() * 2) - 1;  // -1 ~ 1
+  this.velocityOpacity = (Math.random() * 0.1) * -1; // -0.1 ~ 0
+  this.size = Math.random() * 1.5 + 5; // 5 ~ 1.5
+  this.fillColor = fillColor;
+}
+
+// 更新狀態
+Particle.prototype.update = function () {
+  this.x += this.velocityX;
+  this.y += this.velocityY;
+  this.opacity += this.velocityOpacity;
+
+  // 如果粒子超出 canvas 或是 透明度 < 0, 重設為起始狀態
+  if (this.x < 0 || this.x > canvasWidth || this.y < 0 || this.y > canvasHeight || this.opacity < 0) {
+    this.initial(this.fillColor);
+  }
+}
+
+// 畫圓圈
+Particle.prototype.drawCircle = function () {
+  this.context.save();
+
+  this.context.beginPath();
+  this.context.globalAlpha = this.opacity;
+  this.context.fillStyle = this.fillColor;
+  this.context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+  this.context.fill();
+
+  this.context.restore();
+}
+
+// 畫正方形
+Particle.prototype.drawRectangle = function () {
+  this.context.save();
+
+  this.context.beginPath();
+  this.context.save();
+  this.context.globalAlpha = this.opacity;
+  this.context.strokeStyle = this.fillColor;
+  this.context.strokeRect(this.x, this.y, this.size * 1.5, this.size * 1.5);
+
+  this.context.restore();
+}
+
+function initial() {
+  particleList = [];
+  for (var i = 0; i < numberOfParticles; i++) {
+    const randomColor = getRandomGreenColor();
+    particleList.push(new Particle(ctx, randomColor));
+  }
+}
+
+// 開始動畫
+function animate() {
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  for (var i = 0; i < particleList.length; i++) {
+    particleList[i].update();
+    particleList[i].drawCircle();
+  }
+
+  requestId = window.requestAnimationFrame(animate);
+
+}
+
+// 停止動畫
+function stopAnimate() {
+
+  if (requestId) {
+    window.cancelAnimationFrame(requestId);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    requestId = undefined;
+  }
+
+}
+
+var bindButtonEvent = function () {
+  // 滑鼠移進按鈕
+  $('.button-container__btn').on('mouseenter', function () {
+    initial();
+    animate();
+  })
+
+  // 滑鼠移出按鈕
+  $('.button-container__btn').on('mouseleave', function () {
+    stopAnimate();
+  });
+}
+
+
+// 主程式
+bindButtonEvent();
+
+
+// utils
+function getRandomGreenColor() {
+  var color = 'rgb(0, green, 0)';
+  var randomGreen = Math.floor(Math.random() * 155) + 100; // 100 ~ 255
+  return color.replace('green', randomGreen);
+}
+
+function getRandomRedColor() {
+  var color = 'rgb(red, 0, 0)';
+  var randomRed = Math.floor(Math.random() * 155) + 100; // 100 ~ 255
+  return color.replace('red', randomRed);
+}
+
--- canvasLoading/main.js
+++ canvasLoading/main.js
@@ -1,135 +1,85 @@
-
-var Coordinate = function (x, y) {
-  this.x = x;
-  this.y = y;
-}
-
-var Triangele = function (pointA, pointB, pointC, fillStyle) {
-  this.pointA = pointA;
-  this.pointB = pointB;
-  this.pointC = pointC;
-  this.fillStyle = fillStyle;
-}
-
-
 var canvasWidth = 500;
-var canvasHeight = 300;
+var canvasHeight = 500;
 var canvas = $('#canvas')[0];
 var ctx = canvas.getContext('2d');
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
+var $d = $.Deferred();
 
-var triangleList = [];
-var triangleNumber = 10;
-
-var point1A = new Coordinate(-3000 - 250, -50 - 150);
-var point1B = new Coordinate(5000 - 250, -50 - 150);
-var point1C = new Coordinate(250 - 250, 150 - 150);
-
-var point2A = new Coordinate(-2000 - 250, -50 - 150);
-var point2B = new Coordinate(4000 - 250, -50 - 150);
-var point2C = new Coordinate(250 - 250, 150 - 150);
-
-var point3A = new Coordinate(-1500 - 250, -50 - 150);
-var point3B = new Coordinate(3500 - 250, -50 - 150);
-var point3C = new Coordinate(250 - 250, 150 - 150);
-
-var point4A = new Coordinate(-1000 - 250, -50 - 150);
-var point4B = new Coordinate(3000 - 250, -50 - 150);
-var point4C = new Coordinate(250 - 250, 150 - 150);
-
-var point5A = new Coordinate(-500 - 250, -50 - 150);
-var point5B = new Coordinate(2500 - 250, -50 - 150);
-var point5C = new Coordinate(250 - 250, 150 - 150);
-
-
-var point6A = new Coordinate(0 - 250, -50 - 150);
-var point6B = new Coordinate(1200 - 250, -50 - 150);
-var point6C = new Coordinate(250 - 250, 150 - 150);
-
-var point7A = new Coordinate(100 - 250, -50 - 150);
-var point7B = new Coordinate(1100 - 250, -50 - 150);
-var point7C = new Coordinate(250 - 250, 150 - 150);
-
-var point8A = new Coordinate(200 - 250, -50 - 150);
-var point8B = new Coordinate(1000 - 250, -50 - 150);
-var point8C = new Coordinate(250 - 250, 150 - 150);
-
-var point9A = new Coordinate(400 - 250, -50 - 150);
-var point9B = new Coordinate(900 - 250, -50 - 150);
-var point9C = new Coordinate(250 - 250, 150 - 150);
-
-var point10A = new Coordinate(500 - 250, -50 - 150);
-var point10B = new Coordinate(800 - 250, -50 - 150);
-var point10C = new Coordinate(250 - 250, 150 - 150);
-
-var trangleRotateAngle = 0;
-var trangleRotateSpeed = 8;
+var backgroundAngle = 0;
+var gradient = ctx.createConicalGradient(canvasWidth / 2, canvasHeight / 2, -Math.PI, Math.PI);
+gradient.addColorStop(0, '#FF0000');
+gradient.addColorStop(0.9, 'white');
+gradient.addColorStop(1, '#FF0000');
+var filterImage = null;
 
 
 function animate() {
-  trangleRotateAngle += trangleRotateSpeed;
-  trangleRotateAngle = trangleRotateAngle % 360;
+
+  // 60 ~ 180, 240 ~ 360 要旋轉加快
+  if (
+    (backgroundAngle >= 60 && backgroundAngle <= 180) ||
+    (backgroundAngle >= 240 && backgroundAngle <= 360)
+  ) {
+    backgroundAngle += 8;
+  }
+  else {
+    backgroundAngle += 5;
+  }
+
+  // 旋轉超過 360, 歸 0
+  if (backgroundAngle >= 360) {
+    backgroundAngle = 0;
+  }
+
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-  fillEllipse(ctx, 250, 150, 200, 80, '#e31d1a');
+  // 紅色漸層
+  ctx.save();
+  ctx.fillStyle = gradient.pattern;
 
-  fillTriange(ctx, point1A, point1B, point1C, 'rgba(255, 255, 255, 0.05)', trangleRotateAngle);
-  fillTriange(ctx, point2A, point2B, point2C, 'rgba(255, 255, 255, 0.1)', trangleRotateAngle);
-  fillTriange(ctx, point3A, point3B, point3C, 'rgba(255, 255, 255, 0.2)', trangleRotateAngle);
-  fillTriange(ctx, point4A, point4B, point4C, 'rgba(255, 255, 255, 0.2)', trangleRotateAngle);
-  fillTriange(ctx, point5A, point5B, point5C, 'rgba(255, 255, 255, 0.2)', trangleRotateAngle);
-  fillTriange(ctx, point6A, point6B, point6C, 'rgba(255, 255, 255, 0.2)', trangleRotateAngle);
-  fillTriange(ctx, point7A, point7B, point7C, 'rgba(255, 255, 255, 0.2)', trangleRotateAngle);
-  fillTriange(ctx, point8A, point8B, point8C, 'rgba(255, 255, 255, 0.2)', trangleRotateAngle);
-  fillTriange(ctx, point9A, point9B, point9C, 'rgba(255, 255, 255, 0.2)', trangleRotateAngle);
-  fillTriange(ctx, point10A, point10B, point10C, 'rgba(255, 255, 255, 0.5)', trangleRotateAngle);
+  ctx.translate(canvasWidth / 2, canvasHeight / 2);
+  ctx.rotate(backgroundAngle * Math.PI / 180);
+  ctx.translate(-(canvasWidth / 2), -(canvasHeight / 2));
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  ctx.restore();
 
-  fillEllipse(ctx, 250, 150, 100, 65, 'white');
+  // 新光 logo image
+  ctx.drawImage(filterImage, 0, 159, 500, 189);
+
+  // 將 canvas 非 logo 區塊用白色擋住
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvasWidth, 159);
+  ctx.fillRect(0, 348, canvasWidth, 500);
 
   window.requestAnimationFrame(animate);
 }
 
-animate();
+function initial() {
 
-function fillEllipse(context, x, y, a, b, fillStyle) {
-  context.save();
-  var r = (a > b) ? a : b;
-  var ratioX = a / r;
-  var ratioY = b / r;
-  context.scale(ratioX, ratioY);
-  context.beginPath();
-  context.arc(x / ratioX, y / ratioY, r, 0, 2 * Math.PI, false);
-  context.fillStyle = fillStyle;
-  context.closePath();
-  context.fill();
-  context.restore();
+  // 拿到新光 logo 圖片, 存在本地
+  fetchImage(500, 189, './exclude.png').done(function (res) {
+    filterImage = res;
+    animate();
+  });
 }
 
-function fillCircle(context, x, y, radius, fillStyle) {
-  context.save();
-  context.beginPath();
-  context.arc(x, y, radius, 0, 2 * Math.PI);
-  context.fillStyle = fillStyle;
-  context.closePath();
-  context.fill();
-  context.restore();
+// 拿到圖片, return image
+function fetchImage(imageWidth, imageHeight, imageSrc) {
+  var image = new Image(imageWidth, imageHeight);
+  image.onload = drawImageActualSize;
+
+  image.src = imageSrc;
+
+  function drawImageActualSize() {
+
+    $d.resolve(image);
+  }
+
+  return $d.promise();
 }
 
-function fillTriange(context, pointA, pointB, pointC, fillStyle, rotateAngle) {
-  context.save();
-  var path = new Path2D();
-  path.moveTo(pointA.x, pointA.y);
-  path.lineTo(pointB.x, pointB.y);
-  path.lineTo(pointC.x, pointC.y);
-  path.lineTo(pointA.x, pointA.y);
-  context.fillStyle = fillStyle;
+// 主程式
+initial();
 
-  context.setTransform(1, 0, 0, 1, 250, 150);
-  context.rotate(rotateAngle * Math.PI / 180);
-  context.opacity
-  context.fill(path);
-  context.restore();
-
-}
