26f727d7e0bf56b1eb51e3200254ea518d7b53d8
PongPong <neux.jefferypeng@gmail.com>
2021-06-07 02:07:17 +0800

    update

--- canvasLoading/main.js
+++ canvasLoading/main.js
@@ -32,7 +32,7 @@ function animate() {
     (backgroundAngle >= 60 && backgroundAngle <= 180) ||
     (backgroundAngle >= 240 && backgroundAngle <= 360)
   ) {
-    backgroundAngle += 12;
+    backgroundAngle += 13;
   }
   else {
     backgroundAngle += 5;
