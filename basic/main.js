04872f48d6e18734a0d716e5fdb9c833d1143a21
PongPong <neux.jefferypeng@gmail.com>
2021-06-16 00:18:42 +0800

    update basic

--- basic/main.js
+++ basic/main.js
@@ -29,22 +29,38 @@ var requestAnimationFrame =
 var Dot = function () {
 
   // 2. 設定物件初始化的狀態(x, y, size, velocity, size, color...)
-  // TODO
+  this.x = 50;
+  this.y = 50;
+  this.velocity = 1;
+  this.size = 10;
+  this.color = 'black';
 }
 
 // 3. 瀏覽器每次繪製時要 "如何" 操作物件達到想要的效果
 Dot.prototype.update = function () {
 
   // x 往右邊移動
-  // TODO
+  this.x += this.velocity;
 
   // 4. (如果需要) 到達終點時, 將物件設定為起點狀態
   // TODO
+  if (this.x > 300) {
+    this.x = 50;
+  }
 }
 
 // 6. 根據物件狀態, 繪製到 Canvas 上
 Dot.prototype.draw = function () {
-  // TODO
+
+  ctx.save();
+  ctx.beginPath();
+
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+
+  ctx.closePath();
+  ctx.restore();
 }
 
 
@@ -52,7 +68,7 @@ Dot.prototype.draw = function () {
 // 初始化
 function initial() {
   // 初始化一個點
-  // TODO
+  dot = new Dot();
 }
 
 // 更新畫面
@@ -60,16 +76,17 @@ function animate() {
 
   // 5. (如果需要) 每次繪製時的第一步, "擦掉" 畫布的內容
   // 繪製一個跟 canvas 等寬高的白色 rect
-  // TODO
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
   // 更新 dot
-  // TODO
+  dot.update();
 
   // 繪製 dot
-  // TODO
+  dot.draw();
 
   // 瀏覽器準備繪製時, 呼叫要執行的 function
-  // TODO
+  requestAnimationFrame(animate);
 }
 
 // 主程式
