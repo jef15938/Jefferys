80e4fca658aebe71e03981943ccbe23cf6b9cf05
jef15938 <jef15938@gmail.com>
2021-07-03 01:20:22 +0800

    add card for shadow

--- 3DShowCard/main.js
+++ 3DShowCard/main.js
@@ -1,5 +1,6 @@
 var card02 = $('.card-02');
 var imageList = $('.card-02-top__img-list');
+var imageContainer = $('.card-02-top__img-container');
 var title = $('.card-02-bottom__title');
 var description = $('.card-02-bottom__description');
 
@@ -13,14 +14,20 @@ $(card02).on('mouseenter', function () {
   card02[0].style.transition = 'none';
   imageList[0].style.transform = `translateZ(150px)`;
   title[0].style.transform = `translateZ(100px)`;
+  title.addClass('shadow-active');
   description[0].style.transform = `translateZ(100px)`;
+  description.addClass('shadow-active');
+  imageContainer.addClass('shadow-active');
 });
 
 $(card02).on('mouseleave', function () {
   card02[0].style.transition = 'all 0.5s ease-out';
   imageList[0].style.transform = `translateZ(0px)`;
   title[0].style.transform = `translateZ(0px)`;
+  title.removeClass('shadow-active');
   description[0].style.transform = `translateZ(0px)`;
+  description.removeClass('shadow-active');
+  imageContainer.removeClass('shadow-active');
 
   card02[0].style.transform = `rotateX(0deg) rotateY(0deg)`;
 
